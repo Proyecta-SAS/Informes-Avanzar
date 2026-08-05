3cf8c433fd55282a9edaab14bfa508e928868b89
Proyecta-SAS <finetune800@gmail.com>
2026-08-05 15:55:14 -0500

    cambio en estructura

--- db/migrations/007_known_user_email_corrections.sql
+++ db/migrations/007_known_user_email_corrections.sql
@@ -6,4 +6,10 @@ SET email = 'ma.galeano@avanzarsoluciones.com',
 WHERE bitrix_id = '18434'
   AND email IS DISTINCT FROM 'ma.galeano@avanzarsoluciones.com';
 
+UPDATE bitrix.departments
+SET head_bitrix_id = '18434',
+    updated_at = now()
+WHERE id = 1324
+  AND head_bitrix_id IS DISTINCT FROM '18434';
+
 COMMIT;
